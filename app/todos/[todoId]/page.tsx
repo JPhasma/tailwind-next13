9cda39d4b4a9b8ed4ff4791c3489abf3630c57fc
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-11-13 15:59:24 +0000

    13 way of ISR

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -9,7 +9,7 @@ type PageProps = {
 
 const fetchTodo = async (todoId: string) => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+    `https://jsonplaceholder.typicode.com/todos/${todoId}, { next: { revalidate: 60}}`
   );
   const todo: Todo = await res.json();
   return todo;
@@ -30,3 +30,15 @@ async function TodoPage({ params: { todoId } }: PageProps) {
 }
 
 export default TodoPage;
+
+export async function generateStaticParams() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const todos: Todo[] = await res.json();
+
+  // limit for the test api
+  const trimmedTodos = todos.splice(0, 10);
+
+  return trimmedTodos.map((todo) => ({
+    todoId: todo.id.toString(),
+  }));
+}
