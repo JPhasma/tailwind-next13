2775a6d0c6b27b109f7d19c6db433bd6c1d456b6
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-11-13 15:36:40 +0000

    prefecthing the Todos pages

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Todo } from '../../../typings';
 
 type PageProps = {
   params: {
@@ -6,8 +7,26 @@ type PageProps = {
   };
 };
 
-function TodoPage({ params: { todoId } }: PageProps) {
-  return <div>TodoPage: {todoId}</div>;
+const fetchTodo = async (todoId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  );
+  const todo: Todo = await res.json();
+  return todo;
+};
+
+async function TodoPage({ params: { todoId } }: PageProps) {
+  const todo = await fetchTodo(todoId);
+
+  return (
+    <div>
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? 'Yes' : 'No'}</p>
+      <p>By User: {todo.userId}</p>
+    </div>
+  );
 }
 
 export default TodoPage;
